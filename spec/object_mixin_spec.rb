70dfffb6f7f6173f4c1afd940b1ffa797283bfb3
0x1eef <0x1eef@protonmail.com>
2022-03-27 04:27:54 -0300

    specs: update

--- spec/object_mixin_spec.rb
+++ spec/object_mixin_spec.rb
@@ -7,36 +7,39 @@ RSpec.describe Proto::ObjectMixin do
     end
   end
 
-  let(:one) { object.create(nil) { def bar() 42 end } }
+  let(:one) { object.create(nil) { def bar = 42 } }
   let(:two) { object.create(one) }
   let(:three) { object.create(two) }
 
-  context 'when there is one prototype in the chain' do
-    context 'when falling back to the property on the root prototype' do
+  context "when there is one prototype in the chain" do
+    context "when falling back to the property on the root prototype" do
       subject { two.bar }
-
       it { is_expected.to eq(42) }
     end
 
-    context 'when a property is deleted' do
-      before { two.delete('bar') }
+    context "when the property is deleted from the root prototype" do
+      before { two.delete("bar") }
       subject { two.bar }
       it { is_expected.to eq(nil) }
     end
   end
 
-  context 'when there are two prototypes in the chain' do
-    context 'when falling back to the property on the root prototype' do
+  context "when there are two prototypes in the chain" do
+    context "when falling back to the property on the root prototype" do
       subject { three.bar }
-
       it { is_expected.to eq(42) }
     end
 
-    context 'when falling back to the property on the middle prototype' do
-      let(:two) { object.create(one) { def bar() 84 end } }
-
+    context "when falling back to the property on the middle prototype" do
+      let(:two) { object.create(one) { def bar = 84 } }
       subject { three.bar }
       it { is_expected.to eq(84) }
+    end
+
+    context "when the property is deleted from all prototypes in the chain" do
+      before { two.delete("bar") }
+      subject { two.bar }
+      it { is_expected.to eq(nil) }
     end
   end
 end
