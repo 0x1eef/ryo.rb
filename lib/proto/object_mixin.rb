23391ed3cf8cf28e874418c8e3c85dc2810e4a94
0x1eef <0x1eef@protonmail.com>
2022-03-28 06:50:34 -0300

    allow for the superclass to be chosen
    default to Object since it is easier to use than BasicObject, but
    still allow BasicObject be selected as a superclass for those familiar
    with it.

--- lib/proto/object_mixin.rb
+++ lib/proto/object_mixin.rb
@@ -1,6 +1,13 @@
 module Proto::ObjectMixin
-  def create(prototype, &b)
-    Class.new(BasicObject) do
+  ##
+  # @param [Proto, nil]
+  #  The prototype, or nil for none.
+  #
+  # @param [Object,BasicObject] superclass
+  #  The superclass of the object.
+  #  Options are either Object (default) or BasicObject.
+  def create(prototype, superclass: Object, &b)
+    Class.new(superclass) do
       include Proto
       class_eval(&b) if b
     end.new(prototype)
