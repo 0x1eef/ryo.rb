3709b27ea9a58ab81fb8490b6dd0a0f7c28d9135
0x1eef <0x1eef@protonmail.com>
2022-04-08 00:33:46 -0300

    docs: update

--- readme_examples/prototype_inheritance_example.rb
+++ readme_examples/prototype_inheritance_example.rb
@@ -3,23 +3,39 @@ require "ryo/core_ext/object"
 
 ##
 # Create an instance of Object, with no prototype.
-# On this object, define the property "rating".
+# On this object, define the property "eat".
 fruit = Object.create(nil, {
-  eat: lambda { "nomnom" }
+  sour: false,
+  eat: lambda { |fruit| "Eating a #{fruit.name}" },
 })
 
 ##
 # Create a second object, with "fruit" as
 # its prototype. On this object, define
 # the property "name".
-apple = Object.create(fruit, {name: "Apple"})
+apple = Object.create(fruit, {name: "Apple", color: "green"})
 
 ##
-# Query the property "name", and find a match
-# directly on the apple object.
+# Find matches directly on the apple object.
 Kernel.p apple.name # => "Apple"
+Kernel.p apple.color # => "Apple"
 
 ##
-# Query the property "eat", and traverse the
-# prototype chain until it is found on "fruit".
-Kernel.p apple.eat.()
+# Find matches in the prototype chain.
+Kernel.p apple.sour # => false
+Kernel.p apple.eat.(apple)
+
+##
+# Create a third object, with "apple" as its
+# prototype. On this object, define the properties
+# "name" and "sour".
+sour_apple = Object.create(apple, {name: "Sour Apple", sour: true})
+
+##
+# Find matches directly on the sour_apple object.
+Kernel.p sour_apple.name # => "Sour Apple"
+Kernel.p sour_apple.sour # => true
+
+##
+# Find matches in the prototype chain.
+Kernel.p sour_apple.color # => "green"
