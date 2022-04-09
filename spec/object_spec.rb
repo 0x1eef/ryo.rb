48bbdf406e68cd79046d4bacbdba096b400ad64c
0x1eef <0x1eef@protonmail.com>
2022-04-09 06:37:27 -0300

    specs: improve

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe Ryo::ObjectMixin do
 
   shared_examples "the instance methods of Ryo objects" do
     describe "#respond_to?" do
-      context "when a property is defined on self" do
+      context "when a property is defined" do
         before { fruit.bar = 84 }
         subject { fruit.respond_to?(:bar) }
         it { is_expected.to be(true) }
       end
 
-      context "when a property has not been defined" do
+      context "when a property is not defined" do
         subject { fruit.respond_to?(:foobar) }
         it { is_expected.to be(true) }
       end
     end
 
     describe "#method_missing" do
-      context "when querying for a property that does not exist" do
+      context "when a property does not exist" do
         subject { fruit.foobar }
         it { is_expected.to eq(nil) }
       end
@@ -41,12 +41,12 @@ RSpec.describe Ryo::ObjectMixin do
 
   shared_examples "prototype-based inheritance" do
     context "when there is one prototype" do
-      context "when traversing to the property on the root prototype" do
+      context "when traversing to a property on the root prototype" do
         subject { apple.foo }
         it { is_expected.to eq(42) }
       end
 
-      context "when the property is deleted from the root prototype" do
+      context "when a property is deleted from the root prototype" do
         before { Ryo.delete fruit, "foo" }
         subject { apple.foo }
         it { is_expected.to eq(nil) }
@@ -54,18 +54,18 @@ RSpec.describe Ryo::ObjectMixin do
     end
 
     context "when there are two prototypes" do
-      context "when traversing to the property on the root prototype" do
+      context "when traversing to a property on the root prototype" do
         subject { sour_apple.foo }
         it { is_expected.to eq(42) }
       end
 
-      context "when traversing to the property on the middle prototype" do
+      context "when traversing to a property on the middle prototype" do
         let(:apple) { object.create(fruit, foo: 84) }
         subject { sour_apple.foo }
         it { is_expected.to eq(84) }
       end
 
-      context "when the property is deleted from the middle prototype" do
+      context "when a property is deleted from the middle prototype" do
         let(:apple) { object.create(fruit, foo: 84) }
         before { Ryo.delete apple, "foo" }
         subject { sour_apple.foo }
