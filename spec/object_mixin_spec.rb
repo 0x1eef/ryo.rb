67f1d25d2f5e5246ebf8b8a0d90d99905782ee32
0x1eef <0x1eef@protonmail.com>
2022-04-09 06:11:49 -0300

    specs: improve

--- spec/object_mixin_spec.rb
+++ spec/object_mixin_spec.rb
@@ -8,101 +8,101 @@ RSpec.describe Ryo::ObjectMixin do
     end
   end
 
-  let(:one) { create_object.call(nil, foo: 42) }
-  let(:two) { create_object.call(one) }
-  let(:three) { create_object.call(two) }
+  let(:fruit) { create_object.call(nil, foo: 42) }
+  let(:apple) { create_object.call(fruit) }
+  let(:sour_apple) { create_object.call(apple) }
 
   shared_examples "tests" do |superclass|
     context "when there is no prototype in the chain" do
       context "when querying for a property that does not exist" do
-        subject { one.baz }
+        subject { fruit.baz }
         it { is_expected.to eq(nil) }
       end
 
       context "when assigning the same property twice on self" do
-        let(:one) { create_object.call(nil) }
-        before { one.foo = 1 }
-        subject(:assign_second_assignment) { one.foo = 2 }
+        let(:fruit) { create_object.call(nil) }
+        before { fruit.foo = 1 }
+        subject(:assign_second_assignment) { fruit.foo = 2 }
 
         it "avoids defining the getter and setter a second time" do
-          expect(Ryo).to_not receive(:define_method!).with(one, "foo")
-          expect(Ryo).to_not receive(:define_method!).with(one, "foo=")
+          expect(Ryo).to_not receive(:define_method!).with(fruit, "foo")
+          expect(Ryo).to_not receive(:define_method!).with(fruit, "foo=")
           assign_second_assignment
         end
       end
 
       context "when deleting the same property twice from self" do
-        before { Ryo.delete one, "foo" }
-        subject(:perform_second_delete) { Ryo.delete one, "foo" }
+        before { Ryo.delete fruit, "foo" }
+        subject(:perform_second_delete) { Ryo.delete fruit, "foo" }
 
         it "avoids defining the getter a second time" do
-          expect(Ryo).to_not receive(:define_method!).with(one, "foo")
+          expect(Ryo).to_not receive(:define_method!).with(fruit, "foo")
           perform_second_delete
         end
       end
 
       describe "#respond_to?" do
         context "when querying for a property defined by a block" do
-          subject { one.respond_to?(:foo) }
+          subject { fruit.respond_to?(:foo) }
           it { is_expected.to be(true) }
         end
 
         context "when querying for an assigned property on self" do
-          before { one.bar = 84 }
-          subject { one.respond_to?(:bar) }
+          before { fruit.bar = 84 }
+          subject { fruit.respond_to?(:bar) }
           it { is_expected.to be(true) }
         end
 
         context "when querying for a property that does not exist" do
-          subject { one.respond_to?(:foobar) }
+          subject { fruit.respond_to?(:foobar) }
           it { is_expected.to be(true) }
         end
       end
 
       describe "#method_missing" do
         context "when querying for a property that does not exist" do
-          subject { one.foobar }
+          subject { fruit.foobar }
           it { is_expected.to eq(nil) }
         end
       end
     end
 
-    context "when there is one prototype in the chain" do
+    context "when there is fruit prototype in the chain" do
       context "when traversing to the property on the root prototype" do
-        subject { two.foo }
+        subject { apple.foo }
         it { is_expected.to eq(42) }
       end
 
       context "when the property is deleted from the root prototype" do
-        before { Ryo.delete one, "foo" }
-        subject { two.foo }
+        before { Ryo.delete fruit, "foo" }
+        subject { apple.foo }
         it { is_expected.to eq(nil) }
       end
 
       describe "#method_missing" do
         context "when querying for a property that does not exist" do
-          subject { two.foobar }
+          subject { apple.foobar }
           it { is_expected.to eq(nil) }
         end
       end
     end
 
-    context "when there are two prototypes in the chain" do
+    context "when there are apple prototypes in the chain" do
       context "when traversing to the property on the root prototype" do
-        subject { three.foo }
+        subject { sour_apple.foo }
         it { is_expected.to eq(42) }
       end
 
       context "when traversing to the property on the middle prototype" do
-        let(:two) { create_object.call(one, foo: 84) }
-        subject { three.foo }
+        let(:apple) { create_object.call(fruit, foo: 84) }
+        subject { sour_apple.foo }
         it { is_expected.to eq(84) }
       end
 
       context "when the property is deleted from the middle prototype" do
-        let(:two) { create_object.call(one, foo: 84) }
-        before { Ryo.delete two, "foo" }
-        subject { three.foo }
+        let(:apple) { create_object.call(fruit, foo: 84) }
+        before { Ryo.delete apple, "foo" }
+        subject { sour_apple.foo }
         it { is_expected.to eq(42) }
       end
     end
