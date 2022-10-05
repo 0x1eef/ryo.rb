0632217d693720b9df1a2346db6fc942cb8e5034
0x1eef <0x1eef@protonmail.com>
2022-10-05 06:35:30 -0300

    add Ryo::Enumerable#{select,select!}.

--- lib/ryo/enumerable.rb
+++ lib/ryo/enumerable.rb
@@ -6,6 +6,9 @@
 # The methods implemented by this module are available as singleton
 # methods on the {Ryo} module.
 module Ryo::Enumerable
+  include Ryo::Keywords
+  include Ryo::Reflect
+
   ##
   # Iterates over a Ryo object, and yields a key-value pair.
   # When a block is not given, an Enumerator is returned.
@@ -54,11 +57,53 @@ module Ryo::Enumerable
   #  A Ryo object.
   #
   # @return [<Ryo::Object, Ryo::BasicObject>]
-  def map!(ryo, &b)
+  def map!(ryo)
     proto_chain = [ryo, *Ryo.prototype_chain_of(ryo)]
     each(ryo) do |key, value|
       ryo = proto_chain.find { |ryo| Ryo.property?(ryo, key) }
       ryo[key] = yield(key, value)
+    end
+    ryo
+  end
+
+  ##
+  # A specialized implementation of select that performs a filter operation
+  # and returns a *new* Ryo object.
+  #
+  # @param (see #select!)
+  #
+  # @return (see #select!)
+  def select(ryo,  &b)
+    select!(Ryo.dup(ryo), &b)
+  end
+
+  ##
+  # A specialized implementation of select that performs a filter operation
+  # that mutates a Ryo object.
+  #
+  # @example
+  #  point = Ryo(x: 5, y: 5, z: 10)
+  #  point = Ryo({z: 20}, point)
+  #  Ryo.select!(point) { |key, value| %w(x y).include?(key) }
+  #  [point.x, point.y, point.z] # => [5, 5, nil]
+  #
+  # @param [<Ryo::Object, Ryo::BasicObject>] ryo
+  #  A Ryo object.
+  #
+  # @return [<Ryo::Object, Ryo::BasicObject>]
+  def select!(ryo)
+    each_ryo(ryo) do |ryo, key, value|
+      delete(ryo, key) unless yield(key, value)
+    end
+  end
+
+  ##
+  # @private
+  def each_ryo(ryo)
+    [ryo, *prototype_chain_of(ryo)].each do |ryo|
+      properties_of(ryo).each do |key|
+        yield(ryo, key, ryo[key])
+      end
     end
     ryo
   end
