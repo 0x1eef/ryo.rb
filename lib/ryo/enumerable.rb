7bd629c10f5580e52eba8ff9ffa2f70148f97792
0x1eef <0x1eef@protonmail.com>
2022-10-05 08:49:16 -0300

    add Ryo::Enumerable#{reject,reject!}

--- lib/ryo/enumerable.rb
+++ lib/ryo/enumerable.rb
@@ -98,6 +98,37 @@ module Ryo::Enumerable
   end
 
   ##
+  # A specialized implementation of reject that performs a filter operation
+  # and returns a *new* Ryo object.
+  #
+  # @param (see #reject!)
+  #
+  # @return (see #reject!)
+  def reject(ryo, &b)
+    reject!(Ryo.dup(ryo), &b)
+  end
+
+  ##
+  # A specialized implementation of reject that performs a filter operation
+  # that mutates a Ryo object.
+  #
+  # @example
+  #  point = Ryo(x: 1, y: 2, z: 10)
+  #  point = Ryo({z: 5}, point)
+  #  Ryo.reject!(ryo) { |key, value| value > 2 }
+  #  [point.x, point.y, point.z] # => [1, 2, nil]
+  #
+  # @param [<Ryo::Object, Ryo::BasicObject>] ryo
+  #  A Ryo object.
+  #
+  # @return [<Ryo::Object, Ryo::BasicObject>]
+  def reject!(ryo)
+    each_ryo(ryo) do |ryo, key, value|
+      delete(ryo, key) if yield(key, value)
+    end
+  end
+
+  ##
   # @private
   def each_ryo(ryo)
     [ryo, *prototype_chain_of(ryo)].each do |ryo|
