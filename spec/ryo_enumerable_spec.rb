4ffcebb8e8084560600675fb1c75389b7c88b5aa
0x1eef <0x1eef@protonmail.com>
2022-10-08 16:00:51 -0300

    spec: rename let / subject locals.

--- spec/ryo_enumerable_spec.rb
+++ spec/ryo_enumerable_spec.rb
@@ -5,48 +5,48 @@ require_relative "setup"
 RSpec.describe Ryo::Enumerable do
   describe ".each" do
     context "when verifying each traverses through the prototype chain" do
-      subject { Ryo.each(point).map { [_1, _2] } }
-      let(:point_x) { Ryo(x: 0) }
-      let(:point_y) { Ryo({y: 5}, point_x) }
-      let(:point) { Ryo({}, point_y) }
+      subject { Ryo.each(point_c).map { [_1, _2] } }
+      let(:point_a) { Ryo(x: 0) }
+      let(:point_b) { Ryo({y: 5}, point_a) }
+      let(:point_c) { Ryo({}, point_b) }
       it { is_expected.to eq([["y", 5], ["x", 0]]) }
     end
   end
 
   describe ".map" do
-    let(:base) { Ryo::BasicObject(x: 4, y: 4) }
-    let(:point) { Ryo::BasicObject({x: 2, y: 2}, base) }
-    subject(:mpoint) { Ryo.map(point) { _2 * 2 } }
+    let(:point_a) { Ryo::BasicObject(x: 4, y: 4) }
+    let(:point_b) { Ryo::BasicObject({x: 2, y: 2}, point_a) }
+    subject(:point_c) { Ryo.map(point_b) { _2 * 2 } }
 
     context "when verifying the map operation" do
       it { is_expected.to eq({x: 4, y: 4}) }
     end
 
     context "when verifying the map operation on the prototype" do
-      subject { base }
-      before { Ryo.map!(point) { _2 * 2 } }
+      subject { point_a }
+      before { Ryo.map!(point_b) { _2 * 2 } }
       it { is_expected.to eq({x: 8, y: 8}) }
     end
 
     context "when verifying the map operation returns a new object" do
-      subject { Ryo.kernel(:equal?).bind_call(point, mpoint) }
+      subject { Ryo.kernel(:equal?).bind_call(point_b, point_c) }
       it { is_expected.to be(false) }
     end
   end
 
   describe ".select" do
     context "with prototype chain traversal" do
-      subject { Ryo.select(point) { _1 == "y" and _2 == 4 } }
-      let(:base) { Ryo::BasicObject(x: 1, y: 2) }
-      let(:point) { Ryo::BasicObject({x: 3, y: 4}, base) }
+      subject { Ryo.select(point_b) { _1 == "y" and _2 == 4 } }
+      let(:point_a) { Ryo::BasicObject(x: 1, y: 2) }
+      let(:point_b) { Ryo::BasicObject({x: 3, y: 4}, point_a) }
 
       context "when verifying the filter operation" do
         it { is_expected.to eq(y: 4) }
       end
 
       context "when verifying the filter operation on the prototype" do
-        subject { base.y }
-        before { Ryo.select!(point) { _1 == "x" } }
+        subject { point_a.y }
+        before { Ryo.select!(point_b) { _1 == "x" } }
         it { is_expected.to eq(nil) }
       end
     end
@@ -54,33 +54,33 @@ RSpec.describe Ryo::Enumerable do
 
   describe ".reject" do
     context "with prototype chain traversal" do
-      subject { Ryo.reject(point) { _1 == "x" } }
-      let(:base) { Ryo::BasicObject(x: 1, y: 2) }
-      let(:point) { Ryo::BasicObject({x: 3, y: 4}, base) }
+      subject { Ryo.reject(point_b) { _1 == "x" } }
+      let(:point_a) { Ryo::BasicObject(x: 1, y: 2) }
+      let(:point_b) { Ryo::BasicObject({x: 3, y: 4}, point_a) }
 
       context "when verifying the filter operation" do
         it { is_expected.to eq(y: 4) }
       end
 
       context "when verifying the filter operation on the prototype" do
-        subject { base.y }
-        before { Ryo.reject!(point) { _1 == "y" } }
+        subject { point_a.y }
+        before { Ryo.reject!(point_b) { _1 == "y" } }
         it { is_expected.to eq(nil) }
       end
     end
   end
 
   describe ".any?" do
-    let(:base) { Ryo::BasicObject(y: 10) }
-    let(:point) { Ryo::BasicObject({x: 5}, base) }
+    let(:point_a) { Ryo::BasicObject(y: 10) }
+    let(:point_b) { Ryo::BasicObject({x: 5}, point_a) }
 
     context "when an iteration returns a truthy value" do
-      subject { Ryo.any?(point) { _2 > 5} }
+      subject { Ryo.any?(point_b) { _2 > 5} }
       it { is_expected.to be(true) }
     end
 
     context "when an iteration fails to return a truthy value" do
-      subject { Ryo.any?(point) { _2 > 20 } }
+      subject { Ryo.any?(point_b) { _2 > 20 } }
       it { is_expected.to be(false) }
     end
   end
