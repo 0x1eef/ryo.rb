e1937f501a52a884c9b418178148e7768cf32b65
0x1eef <0x1eef@protonmail.com>
2024-06-20 16:31:50 -0300

    Add Rakefile

---
+++ Rakefile.rb
@@ -0,0 +1,13 @@
+require "bundler/setup"
+
+desc "Run CI tasks"
+task :ci do
+  sh "bundle exec rubocop"
+  sh "bundle exec rspec spec/"
+end
+
+desc "Run tests"
+task :test do
+  sh "bundle exec rspec spec/"
+end
+task default: :test
